2521f8d44507a3f41a7c2fd1f9af438001177b7e
margellypach <angelly.monteverde@escola.pr.gov.br>
2025-06-18 14:00:09 -0300

    .

---
+++ perguntas.js
@@ -0,0 +1,35 @@
+criaCartao(
+    'Geografia',
+    'Qual é a capital de Canadá?',
+    'A capital de Canada é Ottawa'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual é a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual é a capital de Espanha?',
+    'A capital de Espanha é Madrid'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual é a capital de Portugal?',
+    'a capital de Portugal é Lisboa'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual é a capital de Coreia do sul?',
+    'A capital de Coreia do Sul é Seul'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual é a capital de China?',
+    'A capital de China é Pequim'
+)
